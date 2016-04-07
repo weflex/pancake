3240213b157c5032952a0fc8f219412844edc812
yorkie <yorkiefixer@gmail.com>
2016-04-07 19:45:15 +0800

    Should cover the duplicated field from existed

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -12,3 +12,10 @@ test('pluralize2', function(t) {
   t.equal(Utils.pluralize2('users'), 'userIds');
   t.end();
 });
+
+test('getModelName', function(t) {
+  t.equal(Utils.getModelName({name: 'WeflexUser', base: 'Model'}), 'weflex-users');
+  t.equal(Utils.getModelName({name: 'WeflexUser', base: 'PersistedModel'}), 'weflex-users');
+  t.equal(Utils.getModelName({name: 'WeflexUser', base: 'User'}), 'users');
+  t.end();
+});
